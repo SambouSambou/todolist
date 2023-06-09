4d9387b714c41b64ca795ee3441b52e40c76c7a3
Stephen Sambou <ssambou@pmoghana.com>
2023-06-09 16:56:32 +0000

    minor changes

--- app.js
+++ app.js
@@ -10,13 +10,34 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {userNewUrlParser: true})
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", 
+{useNewUrlParser: true});
 
 const itemsSchema = {
     name : String
 }
 
-const Item = mongoose.model("Item", "itemsSchema")
+const Item = mongoose.model("Item", itemsSchema);
+const item1 = new Item({
+    name: "welcome to your todolist"
+});
+
+const item2 = new Item({
+    name: "Hit + to enter to add a new item"
+});
+
+const item3 = new Item({
+    name : "Click delete to delete an item"
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems).then(function () {
+    console.log("Successfully saved default items to DB");
+  }).catch(function (err) {
+    console.log(err);
+  });
+  
 
 
 
@@ -28,6 +49,7 @@ app.get('/', function(req, res){
     res.render("list", {
         listTitle: "Today",
         addNewItem: items
+       
     });
 
     
